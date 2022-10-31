d2f3ad7dede3ae9cec29341998abaa8b63367b7d
andreeascortan28 <olivera.scortan@student.upt.ro>
2022-10-31 08:25:02 +0200

    Working on backend

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,29 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
 
-const app = express()
+const app = express();
+
+//Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+//Routes middleware
+app.use("/api/users", userRoute);
+
+//Routes
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
+
+//Error middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
