1e97aedfb8ca9843adc4373a70b51233e472d573
andreeascortan28 <olivera.scortan@student.upt.ro>
2022-11-06 13:41:26 +0200

    Profile + edit profile done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,8 @@ import { SET_LOGIN } from "./redux/features/auth/authSlice";
 import AddProduct from "./pages/addProduct/AddProduct";
 import ProductDetail from "./components/product/productDetail/ProductDetail";
 import EditProduct from "./pages/editProduct/EditProduct";
+import Profile from "./pages/profile/Profile";
+import EditProfile from "./pages/profile/EditProfile";
 //import ProductDetail from "./components/product/productDetail/ProductDetail";
 //import EditProduct from "./pages/editProduct/EditProduct";
 // import Profile from "./pages/profile/Profile";
@@ -82,6 +84,26 @@ function App() {
             <Sidebar>
               <Layout>
                 <EditProduct />
+              </Layout>
+            </Sidebar>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <Sidebar>
+              <Layout>
+                <Profile />
+              </Layout>
+            </Sidebar>
+          }
+        />
+        <Route
+          path="/edit-profile"
+          element={
+            <Sidebar>
+              <Layout>
+                <EditProfile />
               </Layout>
             </Sidebar>
           }
